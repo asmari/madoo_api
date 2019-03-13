78e86e668da9ddc3f0569e14a4ce8caad8d5cabd
Bowo <patranto.prabowo@gmail.com>
2019-03-13 12:07:57 +0700

    add schema for login and check member to route login and check member

--- routes/version-1/auth.js
+++ routes/version-1/auth.js
@@ -1,14 +1,15 @@
 const authController = require("../../controller/version-1/authController")
+const authSchema = require("../../schema/authSchema")
 
 async function routes(fastify, options) {
     // get members
     fastify.get('/', authController.authIndex)
 
     // login members
-    fastify.post('/login', authController.doLogin)
+    fastify.post('/login',authSchema.authLoginSchema, authController.doLogin)
 
     // check member
-    fastify.post("/check", authController.doCheckMember)
+    fastify.post("/check", authSchema.authCheckSchema, authController.doCheckMember)
 }
 
 module.exports = routes
