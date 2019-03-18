5ab110c599985d4654c8b9216a02129298614550
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-03-18 17:15:15 +0700

    add models MemberCards, Loyalty, LoyaltyType, LoyaltyMemberCards link to model index

--- models/index.js
+++ models/index.js
@@ -1,2 +1,6 @@
 exports.Members = require('./members');
 exports.Pins = require('./pin_members');
+exports.MembersCards = require("./member_cards")
+exports.Loyalty = require("./loyalty")
+exports.LoyaltyType = require("./type_loyalty")
+exports.LoyaltyMemberCards = require("./loyalty_member_cards")
