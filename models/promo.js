9f31c23160f8f8dce7a0f69f49ca4c45a8183e00
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-03-19 11:56:13 +0700

    create promo model

---
+++ models/promo.js
@@ -0,0 +1,41 @@
+const sequelize = require("sequelize")
+const connect = require("./conn/sequelize")
+const model = connect.sequelize
+
+
+const Promo = model.define("promo",{
+    loyalty_id:{
+        type:sequelize.STRING,
+        allowNull:false,
+        validate:{
+            notEmpty:{
+                msg: "Loyalty id is required"
+            }
+        }
+    },
+    title:{
+        type:sequelize.STRING,
+        allowNull:false
+    },
+    body:{
+        type:sequelize.STRING,
+        allowNull:false
+    },
+    image:{
+        type:sequelize.STRING,
+        allowNull:true
+    },
+    valid_until:{
+        type:sequelize.DATE,
+        allowNull:false
+    }
+},{
+    timestamps:true,
+    underscored:true,
+    deletedAt:"deleted_at",
+    paranoid:true,
+    freezeTableName:true,
+    tableName:"promo",
+})
+
+exports.Get = Promo
