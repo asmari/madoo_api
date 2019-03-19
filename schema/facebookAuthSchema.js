c6378e1831ba3d862d3e4a8c1f4e6ba732ef3b8c
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-03-19 16:00:15 +0700

    remove country code from facebook register

--- schema/facebookAuthSchema.js
+++ schema/facebookAuthSchema.js
@@ -16,7 +16,7 @@ exports.facebookRegisterSchema = {
     schema:{
         consumes:["multipart/form-data"],
         body:{
-            required:["full_name", "email", "fb_id", "fb_token", "mobile_phone","country_code", "pin"],
+            required:["full_name", "email", "fb_id", "fb_token", "mobile_phone", "pin"],
             properties:{
                 full_name:{type:"string"},
                 email:{type:"string"},
@@ -28,7 +28,6 @@ exports.facebookRegisterSchema = {
                 fb_id:{
                     type:"integer"},
                 fb_token:{type:"integer"},
-                country_code:{type:"string"},
                 mobile_phone:{type:"integer"},
                 pin:{type:"integer"},
                 fingerprint:{type:"integer"}
