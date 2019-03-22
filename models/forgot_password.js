2906d5de0ffd2fc99b9212b84111f6195ce0df81
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-03-22 10:38:42 +0700

    create model forgot password

---
+++ models/forgot_password.js
@@ -0,0 +1,60 @@
+const sequilize = require("sequelize")
+const connect = require("./conn/sequelize")
+const model = connect.sequelize
+
+
+const ForgotPassword = model.define("forgot_password", {
+    members_id:{
+        type:sequilize.INTEGER,
+        allowNull:false,
+        maxLength:10
+    },
+    uid:{
+        type:sequilize.STRING,
+        allowNull:false,
+        maxLength:150
+    },
+    resourceUri:{
+        type:sequilize.STRING,
+        allowNull:false
+    },
+    msisdn:{
+        type:sequilize.STRING,
+        allowNull:false,
+        maxLength:20
+    },
+    status:{
+        type:sequilize.STRING,
+        allowNull:false,
+        maxLength:15
+    },
+    attempt:{
+        type:sequilize.INTEGER,
+        allowNull:false,
+        maxLength:10
+    },
+    expiresAt:{
+        type:sequilize.STRING,
+        allowNull:false,
+        maxLength:100
+    },
+    nextSmsAfter:{
+        type:sequilize.STRING,
+        allowNull:false,
+        maxLength:100
+    },
+    error_message:{
+        type:sequilize.STRING,
+        allowNull:true
+    }
+},{
+    timestamps:true,
+    paranoid:true,
+    deletedAt:"deleted_at",
+    underscored:true,
+    freezeTableName:true,
+    tableName:"forgot_password"
+})
+
+
+exports.Get = ForgotPassword
