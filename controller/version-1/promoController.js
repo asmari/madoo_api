a18b980563290eedccf12215753c7e9a0267ce68
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-03-25 14:32:16 +0700

    add promo filter

--- controller/version-1/promoController.js
+++ controller/version-1/promoController.js
@@ -11,12 +11,40 @@ const Promo = model.Promo.Get
 exports.getRandomPromo = async (request, reply) => {
     try{
 
-        const currentDate= moment().format('YYYY-MM-DD');
+        const params = JSON.parse(JSON.stringify(request.query))
+
+        let filterPromo = {}
+
+        if(params.hasOwnProperty("filter")){
+
+            let filter = params.filter
+
+            if(typeof(params.filter) == "number"){
+                filter = [params.filter]
+            }
+
+            filterPromo = {
+                id:{
+                    [Op.in]:filter
+                }
+            }
+        }
+
+
+        const currentDate = moment().format('YYYY-MM-DD');
         // return reply.code(200).send(helper.Success(currentDate))
-        Promo.findAll({where:sequelize.where(sequelize.fn('date', sequelize.col('valid_until')), '>=', currentDate),
-            include: [Loyalty], limit: 10 ,
+        Promo.findAll({
+            where:{
+                ...filterPromo,
+                // sequelize.where(sequelize.fn('date', sequelize.col('valid_until')), '>=', currentDate)
+                valid_until:{
+                    [Op.gte]:currentDate
+                }
+            },
+            include: [Loyalty], 
+            limit: 10 ,
             order: sequelize.literal('rand()')})
-            .then(promos=>{
+        .then(promos=>{
             return reply.code(200).send(helper.Success(promos))
         });
 
