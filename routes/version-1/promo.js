94f70507ba8676a26d18a9eba3f55a851fce219f
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-03-25 14:31:52 +0700

    add schema to route promo

--- routes/version-1/promo.js
+++ routes/version-1/promo.js
@@ -1,11 +1,11 @@
 const promoController  = require("../../controller/version-1/promoController")
-
+const promoSchema = require("../../schema/promoSchema")
 
 async function routes (fastify, options) {
 
 
     //list random promo
-    fastify.get("/list/random", promoController.getRandomPromo)
+    fastify.get("/list/random", promoSchema.promoListSchema, promoController.getRandomPromo)
 
 
 }
