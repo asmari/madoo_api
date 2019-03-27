3967d1dc79d2ff5d766977a3a9f8542d3042b305
asmari07 <asmari@indosystem.com>
2019-03-27 14:20:57 +0700

    create or update fcm token

--- routes/version-1.js
+++ routes/version-1.js
@@ -1,25 +1,25 @@
 async function routes(fastify, options) {
-    fastify.get('/', { schema: {hide: true}}, async function(request, reply) {
-        return reply.code(200).send({ hello: 'fuad' });
-    });
+	fastify.get('/', { schema: { hide: true } }, async (request, reply) => reply.code(200).send({ hello: 'fuad' }));
 
-    fastify.register(require('./version-1/sample'), { prefix: '/sample' })
+	fastify.register(require('./version-1/sample'), { prefix: '/sample' });
 
-    fastify.register(require('./version-1/auth'), { prefix: '/auth' })
+	fastify.register(require('./version-1/auth'), { prefix: '/auth' });
 
-    fastify.register(require('./version-1/members'), { prefix: '/members' })
+	fastify.register(require('./version-1/members'), { prefix: '/members' });
 
-    fastify.register(require("./version-1/loyalty"), { prefix: '/loyalty' })
+	fastify.register(require('./version-1/loyalty'), { prefix: '/loyalty' });
 
-    fastify.register(require("./version-1/promo"), { prefix: '/promo' })
+	fastify.register(require('./version-1/promo'), { prefix: '/promo' });
 
-    fastify.register(require("./version-1/facebook"), { prefix: "/facebook" })
+	fastify.register(require('./version-1/facebook'), { prefix: '/facebook' });
 
-    fastify.register(require("./version-1/google"), { prefix: '/google' })
+	fastify.register(require('./version-1/google'), { prefix: '/google' });
 
-    fastify.register(require("./version-1/convertion"), { prefix : '/conversion' })
+	fastify.register(require('./version-1/convertion'), { prefix: '/conversion' });
 
-    fastify.register(require("./version-1/masterRoute"), { prefix: "/master" })
+	fastify.register(require('./version-1/masterRoute'), { prefix: '/master' });
+
+	fastify.register(require('./version-1/notification'), { prefix: '/notification' });
 }
 
-module.exports = routes
+module.exports = routes;
---
+++ schema/notificationSchema.js
@@ -0,0 +1,32 @@
+exports.fcmTokenSchema = {
+	schema: {
+		security: [
+			{
+				BearerAuth: [],
+			},
+		],
+		querystring: {
+			type: 'object',
+			properties: {
+				members_id: {
+					type: 'integer',
+				},
+				device_id: {
+					type: 'integer',
+				},
+				fcm_token: {
+					type: 'string',
+				},
+				device_type: {
+					type: 'string',
+				},
+				device_info: {
+					type: 'string',
+				},
+			},
+			required: ['members_id', 'fcm_token', 'device_id'],
+		},
+
+	},
+
+};
