c391a4a83bbd0e05b72127d0bdc61559247a699e
fuad <fuad.suyudi@indosystem.com>
2019-04-08 10:47:53 +0700

    update

--- routes/version-1/notification.js
+++ routes/version-1/notification.js
@@ -1,11 +1,9 @@
 const notificationSchema = require('../../schema/notificationSchema');
 const notificationController = require('../../controller/version-1/notificationController');
 
-async function routes(fastify, options) {
-
+async function routes(fastify) {
 	// register or update token
 	fastify.post('/token', notificationSchema.fcmTokenSchema, notificationController.doRegisterToken);
-
 }
 
 module.exports = routes;
