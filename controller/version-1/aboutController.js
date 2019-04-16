e7956c7bb399b54144fdd7da759b1166400d6729
asmari07 <asmari@indosystem.com>
2019-04-16 11:05:18 +0700

    remove pagination on about & add schema description

--- controller/version-1/aboutController.js
+++ controller/version-1/aboutController.js
@@ -1,31 +1,14 @@
-const { ResponsePaginate } = require('../../helper/response');
+const { Response } = require('../../helper/response');
 const model = require('../../models');
 
 const About = model.About.Get;
 
 // get list contact
-exports.getAbout = async (request) => {
-	const params = {
-		page: parseInt(request.query.page, 10) || 1,
-		item: parseInt(request.query.item, 10) || 10,
-		search: request.query.search || null,
-		sort: request.query.sort || null,
-		filter: request.query.filter || [],
-		total: 0,
-	};
+exports.getAbout = async () => {
+	const abouts = await About.findOne({
+		limit: 1,
+		order: [['updated_at', 'DESC']],
+	});
 
-	const dataOptions = {
-		page: params.page,
-		paginate: params.item,
-	};
-
-	const abouts = await About.paginate(dataOptions);
-
-	const data = abouts.docs;
-
-	return new ResponsePaginate(20034, {
-		item: params.item,
-		pages: params.page,
-		total: abouts.total,
-	}, data);
+	return new Response(20034, abouts);
 };
---
+++ schema/staticContent.js
@@ -0,0 +1,10 @@
+exports.aboutSchema = {
+	schema: {
+		description: 'Rest API for about',
+	},
+};
+exports.contactSchema = {
+	schema: {
+		description: 'Rest API for contact',
+	},
+};
