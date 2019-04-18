02402e1b69f4f54587af2319f53171d6ebe92d07
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-04-18 12:28:05 +0700

    add route notification trigger

--- routes/version-1/notification.js
+++ routes/version-1/notification.js
@@ -4,6 +4,9 @@ const notificationController = require('../../controller/version-1/notificationC
 async function routes(fastify) {
 	// register or update token
 	fastify.post('/token', notificationSchema.fcmTokenSchema, notificationController.doRegisterToken);
+
+	// trigger notification
+	fastify.get('/trigger', notificationSchema.fcmTriggerSchema, notificationController.doSendNotification);
 }
 
 module.exports = routes;
