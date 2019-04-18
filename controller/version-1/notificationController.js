c47f4e74eb0ceb0f57ed1a25a1e772f5a51d7460
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-04-18 12:26:24 +0700

    add function controller for handling trigger notification

--- controller/version-1/notificationController.js
+++ controller/version-1/notificationController.js
@@ -1,8 +1,30 @@
+// eslint-disable-next-line import/no-unresolved
+const { Worker } = require('worker_threads');
+const path = require('path');
+
 const { Response } = require('../../helper/response');
 const model = require('../../models');
 
-
 const DeviceNotification = model.DeviceNotification.Get;
+
+
+// send notification
+exports.doSendNotification = async (request) => {
+	const params = JSON.parse(JSON.stringify(request.query));
+
+	const worker = new Worker(path.resolve(__dirname, '../../services/notificationServices.js'), {
+		workerData: {
+			id: params.notification_id,
+			memberId: params.members_id || 0,
+		},
+	});
+
+	// eslint-disable-next-line no-console
+	console.log(worker);
+
+	return new Response(20035);
+};
+
 // save notification token
 exports.doRegisterToken = async (request) => {
 	const params = JSON.parse(JSON.stringify(request.query));
