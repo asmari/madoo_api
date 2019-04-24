2818186c06422e97e7e7b12fa85b864268b95904
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-04-24 10:22:22 +0700

    set limit mobile_phone from 11 to 9

--- schema/memberSchema.js
+++ schema/memberSchema.js
@@ -20,7 +20,7 @@ exports.otpSchema = {
 			// Required body paramter for register
 			required: ['mobile_phone', 'otp'],
 			properties: {
-				mobile_phone: { type: 'string', maxLength: 20, minLength: 11 },
+				mobile_phone: { type: 'string', maxLength: 20, minLength: 9 },
 				otp: { type: 'integer', maxLength: 6, minLength: 6 },
 
 			},
@@ -37,7 +37,7 @@ exports.memberSchema = {
 			properties: {
 				full_name: { type: 'string', maxLength: 50, minLength: 3 },
 				email: { type: 'string', maxLength: 50, minLength: 3 },
-				mobile_phone: { type: 'string', maxLength: 20, minLength: 11 },
+				mobile_phone: { type: 'string', maxLength: 20, minLength: 9 },
 				pin: { type: 'string', maxLength: 6, minLength: 6 },
 				fingerprint: { type: 'integer', default: 0 },
 			},
