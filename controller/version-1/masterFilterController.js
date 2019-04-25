140bd697aa1948410e46520ea5023e953ab3cd58
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-04-25 13:09:02 +0700

    change promo list master

--- controller/version-1/masterFilterController.js
+++ controller/version-1/masterFilterController.js
@@ -3,59 +3,25 @@ const { Op } = require('sequelize');
 const { ErrorResponse, Response } = require('../../helper/response');
 const {
 	Loyalty,
+	LoyaltyType,
 	LoyaltyMemberCards,
 	MembersCards,
-	Promo,
 } = require('../../models');
 
 // list filter promo content
-exports.getFilterListPromo = async (request) => {
-	const { user } = request;
-
-	const memberCards = await MembersCards.Get.findAll({
-		where: {
-			members_id: user.id,
-		},
+exports.getFilterListPromo = async () => {
+	const category = await LoyaltyType.Get.findAll({
+		attributes: ['id', 'title'],
 	});
 
-	if (memberCards) {
-		const memberCardsId = memberCards.map(value => value.id);
+	const loyalty = await Loyalty.Get.findAll({
+		attributes: ['id', 'name'],
+	});
 
-		const promo = await Promo.Get.findAll({
-			loyalty_id: {
-				[Op.in]: memberCardsId,
-			},
-		});
-
-		const promoMemberId = promo.map(value => value.loyalty_id);
-
-		const loyaltyMemberCards = await LoyaltyMemberCards.Get.findAll({
-			where: {
-				member_cards_id: {
-					[Op.in]: promoMemberId,
-				},
-			},
-			include: [
-				{
-					model: Loyalty.Get,
-				},
-			],
-		});
-
-		const data = loyaltyMemberCards.map((value) => {
-			const innerData = value.loyalties.map(value2 => ({
-				id: value2.id,
-				name: value2.name,
-			}));
-
-			return innerData[0] || [];
-		});
-
-		return new Response(20016, data);
-	}
-
-	// Error: Member cards not found
-	throw new ErrorResponse(41705);
+	return new Response(20016, {
+		category,
+		loyalty,
+	});
 };
 
 // list sort card content
