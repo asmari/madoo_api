dd7549aeb9b83dfcb345813f9a129afbb5d2c312
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-04-25 09:40:40 +0700

    change contact from list to one json

--- controller/version-1/contactController.js
+++ controller/version-1/contactController.js
@@ -1,31 +1,15 @@
-const { ResponsePaginate } = require('../../helper/response');
+const { Response, ErrorResponse } = require('../../helper/response');
 const model = require('../../models');
 
 const Contact = model.Contact.Get;
 
 // get list contact
-exports.getContact = async (request) => {
-	const params = {
-		page: parseInt(request.query.page, 10) || 1,
-		item: parseInt(request.query.item, 10) || 10,
-		search: request.query.search || null,
-		sort: request.query.sort || null,
-		filter: request.query.filter || [],
-		total: 0,
-	};
+exports.getContact = async () => {
+	const contacts = await Contact.findAll();
 
-	const dataOptions = {
-		page: params.page,
-		paginate: params.item,
-	};
+	if (contacts.length > 0) {
+		return new Response(20033, contacts[0]);
+	}
 
-	const contacts = await Contact.paginate(dataOptions);
-
-	const data = contacts.docs;
-
-	return new ResponsePaginate(20033, {
-		item: params.item,
-		pages: params.page,
-		total: contacts.total,
-	}, data);
+	return new ErrorResponse(41707);
 };
