e3d5fc49d80e6a903d8a570430180a078a583411
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-05-08 09:56:20 +0700

    fixing pin wrong member 2 times

--- controller/version-1/authController.js
+++ controller/version-1/authController.js
@@ -98,7 +98,9 @@ exports.doLogin = async (request, reply) => {
 
 	if (pin) {
 		const pinMember = await Pins.findOne({
-			members_id: member.id,
+			where: {
+				members_id: member.id,
+			},
 		});
 
 		const wrong = pinMember.wrong == null ? 0 : pinMember.wrong;
