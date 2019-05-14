ea2d6f07753c2436cf5a164e8ac7e8e2ee933f94
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-05-14 14:53:22 +0700

    create new model for table member_card_auth_tokens

---
+++ models/memberCardsAuthToken.js
@@ -0,0 +1,26 @@
+const sequelize = require('sequelize');
+const model = require('./conn/sequelize').sequelize;
+
+const MemberCardsAuthToken = model.define('member_card_auth_tokens', {
+	members_id: {
+		type: sequelize.INTEGER,
+	},
+	member_cards_id: {
+		type: sequelize.INTEGER,
+	},
+	type_auth: {
+		type: sequelize.STRING,
+	},
+	auth_value: {
+		type: sequelize.TEXT,
+	},
+}, {
+	timestamps: true,
+	underscored: true,
+	paranoid: true,
+	deletedAt: 'deleted_at',
+	freezeTableName: true,
+	tableName: 'member_card_auth_tokens',
+});
+
+exports.Get = MemberCardsAuthToken;
