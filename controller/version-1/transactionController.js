d26add1451293dbf1502b158908017ad70c8febf
Patranto Prabowo <patranto.prabowo@indosystem.com>
2019-06-11 16:05:25 +0700

    change transaction to new format response

--- controller/version-1/transactionController.js
+++ controller/version-1/transactionController.js
@@ -6,7 +6,10 @@ const { ErrorResponse, ResponsePaginate } = require('../../helper/response');
 const Transaction = model.Transaction.Get;
 const MemberCards = model.MembersCards.Get;
 const LoyaltyMemberCards = model.LoyaltyMemberCards.Get;
+const Loyalty = model.Loyalty.Get;
 const Members = model.Members.Get;
+
+const monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 // get history transaction
 exports.doGetListHistory = async (request) => {
@@ -64,18 +67,96 @@ exports.doGetListHistory = async (request) => {
 					model: MemberCards,
 					as: 'source_member_cards',
 					where: whereOptions,
+					include: [
+						{
+							model: LoyaltyMemberCards,
+							include: [
+								{
+									model: Loyalty,
+									attributes: ['name', 'unit'],
+								},
+							],
+						},
+					],
 				},
 				{
 					model: MemberCards,
 					as: 'target_member_cards',
+					include: [
+						{
+							model: LoyaltyMemberCards,
+							include: [
+								{
+									model: Loyalty,
+									attributes: ['name', 'unit'],
+								},
+							],
+						},
+					],
 				},
 			],
+		});
+
+		const responseData = trxList.docs.map((value) => {
+			const dVal = [];
+
+			const d = value.toJSON();
+			delete d.source_member_cards;
+			delete d.target_member_cards;
+
+			const listField = ['source', 'target'];
+
+			const time = new Date(value.created_at);
+
+			d.transaction_date = `${time.getDate()} ${monthList[time.getMonth()]} ${time.getFullYear()}`;
+
+			listField.forEach((key) => {
+				const reVal = {
+					id: d.id,
+					transaction_date: d.transaction_date,
+					transaction_type: 'Point Conversion',
+					unix_id: d.unix_id,
+					status: d.status,
+					point: key === 'source' ? -d.point : d.conversion_point,
+					loyalty_unit: '',
+					loyalty: '',
+				};
+
+				if (Object.prototype.hasOwnProperty.call(value, `${key}_member_cards`) && value[`${key}_member_cards`].length > 0) {
+					const sourceLoyalty = value[`${key}_member_cards`][0].toJSON();
+
+					reVal.loyalty = 'Not Found';
+					reVal.loyalty_unit = '-';
+
+					if (Object.prototype.hasOwnProperty.call(sourceLoyalty, 'loyalty_has_member_cards') && sourceLoyalty.loyalty_has_member_cards.length > 0) {
+						const loyaltyCard = sourceLoyalty.loyalty_has_member_cards[0];
+
+						if (Object.prototype.hasOwnProperty.call(loyaltyCard, 'loyalties') && loyaltyCard.loyalties.length > 0) {
+							const loyalty = loyaltyCard.loyalties[0];
+							reVal.loyalty = loyalty.name;
+							reVal.loyalty_unit = loyalty.unit;
+						}
+					}
+				}
+
+				dVal.push(reVal);
+			});
+
+			return dVal;
+		});
+
+		const resFinal = [];
+
+		responseData.forEach((v) => {
+			v.forEach((v1) => {
+				resFinal.push(v1);
+			});
 		});
 
 		return new ResponsePaginate(20030, {
 			...trxList,
 			item: params.item,
-		});
+		}, resFinal);
 	} catch (err) {
 		console.log(err);
 		throw new ErrorResponse(41798, {
