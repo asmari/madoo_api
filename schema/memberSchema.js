6f47b8f9b506ed70b10b2679b9a21cc1e2e39b4c
Bowo <patranto.prabowo@indosystem.com>
2019-07-18 10:58:11 +0700

    add schema remove token

--- schema/memberSchema.js
+++ schema/memberSchema.js
@@ -164,6 +164,25 @@ exports.sendOtpUpdateSchema = {
 	},
 };
 
+exports.removeTokenSchema = {
+	schema: {
+		security: [
+			{
+				'skip-auth': [],
+			},
+		],
+		description: 'Rest API for remove token user',
+		body: {
+			required: ['user_id'],
+			properties: {
+				user_id: {
+					type: 'integer',
+				},
+			},
+		},
+	},
+};
+
 exports.checkOtpUpdateSchema = {
 	schema: {
 		security: [
