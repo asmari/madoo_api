2d39d56cd8138e304400a3ffada601198655d501
fuad <fuad.suyudi@indosystem.com>
2019-08-13 14:04:44 +0700

    update fee idr

--- controller/version-1/convertionController.js
+++ controller/version-1/convertionController.js
@@ -390,7 +390,7 @@ exports.doConvertionPoint = async (request) => {
 			conversion_point_balance_after: (cardTarget.point_balance + pointWithFee),
 			status: 'pending',
 			fee,
-			feeidr: feeIdr,
+			// feeidr: feeIdr,
 			mid_rate_from: rate.mid_from_rate,
 			mid_rate_to: rate.mid_to_rate,
 		});
